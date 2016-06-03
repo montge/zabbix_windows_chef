13fb9740a45b998de2cc6f0471606cb5ba998808
Evan Montgomery-Recht <montgomery-recht_evan@bah.com>
2016-06-02 20:55:44 -0400

    Fixed Zabbix 2.2.9 versions

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "thomasvincent@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures zabbix client on Windows."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 name		 "zabbix_windows"
